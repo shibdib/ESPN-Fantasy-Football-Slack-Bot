80de3e0359b8632506fb9b52218cdfa4b7ae92ae
shibdib <supernfadmin@gmail.com>
2019-07-12 05:54:19 -0400

    add about plugin

--- plugins/about.js
+++ plugins/about.js
@@ -4,7 +4,7 @@ module.exports.plugin = function (espn, web, event) {
             text: '*ESPN FF Slack Bot*' + '\n' +
             ' _Creator_ - Shibdib (Bob Sardinia)' + '\n' +
             ' _Current Plugins_ - Player Lookup (!p player_name), About (!a)' + '\n' +
-            '<https://github.com/shibdib/ESPN-Slack-FF' + found.player.id + '/|Github Link>'
+            '<https://github.com/shibdib/ESPN-Slack-FF|Github Link>'
         });
     })();
 };
